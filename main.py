00b5dc7af6c171d6e5fb268860f71b6808b10edc
tedirland <irlandth@gmail.com>
2024-01-23 07:36:07 -0600

    Added in message history using ConversationBufferMemory class

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
+    input_variables=["content", "messages"],
     messages= [
+        MessagesPlaceholder(variable_name='messages'),
         HumanMessagePromptTemplate.from_template("{content}")
     ]
 )
 
 chain = LLMChain(
     llm=chat,
-    prompt=prompt
+    prompt=prompt,
+    memory=memory
 )
 
 while True:
